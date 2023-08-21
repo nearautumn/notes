d1d66409287b76962c5b878edd085424157f7d8d
Andrei Aliaksandrau <aleksandrov.a.95@yandex.ru>
2023-08-21 19:43:14 +0300

    showing notes fucntion updated

--- modules.py
+++ modules.py
@@ -1,10 +1,15 @@
-def show_notes(file: str):
-
+def read_file(file: str) -> list:
     with open(file, mode='r', encoding='utf-8') as notes_file:
         ls = []
         for line in notes_file:
             ls.append(line.strip())
-        user_view = [d.replace(';', '|') for d in ls]
-        print(*user_view, sep='\n')
+        return ls
+
+
+def show_notes(file: str):
+    list_of_rows = read_file(file)
+    user_view = [d.replace(';', '|') for d in list_of_rows]
+    print(*user_view, sep='\n')
+
 
 
